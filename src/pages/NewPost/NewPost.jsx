feea54420dbc0c2e76fdd46256dee5cf5c29db66
Noorullah Noori <164587782+noorinoorullah130@users.noreply.github.com>
2025-03-28 12:02:42 +0430

    new post page is complete

--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+
+import "./NewPost.css";
+import Header from "../../components/Header/Header";
+import Left from "./../../components/Left/Left";
 
 const NewPost = () => {
-  return (
-    <div>NewPost</div>
-  )
-}
+    return (
+        <>
+            <Header />
+            <Left />
+            <div className="new-post">
+                <form className="post-form">
+                    <h1>Create New Post</h1>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        type="text"
+                        id="title"
+                        placeholder="Enter post title..."
+                    />
 
-export default NewPost
+                    <label htmlFor="post-content">Post Content</label>
+                    <textarea
+                        placeholder="Enter post Content"
+                        id="post-content"
+                        rows={5}
+                    ></textarea>
+
+                    <label htmlFor="upload-image">Upload Image</label>
+                    <input type="file" id="upload-image" />
+
+                    <button type="submit">Save</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default NewPost;
